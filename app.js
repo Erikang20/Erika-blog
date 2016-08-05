0be7f999744dcfb8fd68635b0ae902ca53f449e1
Erikang20 <erikang20@gmail.com>
2016-08-04 19:14:11 -0500

    add changes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 "use strict";
 var express = require( 'express' );
 var path = require( 'path' );
+var cookieParser = require( 'cookie-parser' );
 var cookieSession = require( 'cookie-session' );
 var knex = require( 'knex' );
 var pg = require( 'pg' );
-// morgan = require( 'morgan' ),
+var logger = require( 'morgan' );
 var methodOverride = require( 'method-override' );
 var bodyParser = require( 'body-parser' );
 var routes = require( './routes/index' );
@@ -14,7 +15,7 @@ var app = express();
 // require( 'locus' );
 require( 'dotenv' ).config();
 
-
+app.use( logger( 'dev' ) );
 app.set( 'view engine', 'ejs' );
 app.set( 'views', ( __dirname + '/views' ) );
 app.use( express.static( __dirname + '/public' ) );
@@ -23,6 +24,7 @@ app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( {
     extended: false
 } ) );
+app.use( cookieParser() );
 app.use( methodOverride( '_method' ) );
 
 app.use( '/', routes );
